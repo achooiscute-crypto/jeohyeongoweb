d912085c5d28390e0db0b68d0540786aeef87d7f
jihoo08 <jihu5701422@gmail.com>
2025-12-15 10:29:24 +0900

    이전까지는 ㄱㅊ

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -224,7 +224,7 @@ def show_login_page():
             st.markdown("""
             <div style='text-align: center; margin-bottom: 20px;'>
                 <h1 style='color: white; text-shadow: 2px 2px 4px rgba(0,0,0,0.5);'>
-                     저현고 학술제
+                     
                 </h1>
             </div>
             """, unsafe_allow_html=True)
